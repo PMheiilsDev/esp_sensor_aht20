111b6cf83466e89ce243cddaecbe24f2db7ce394
PM <>
2026-08-14 20:03:02 +0200

    fix for reciever

--- pi/reciever.py
+++ pi/reciever.py
@@ -5,22 +5,33 @@ from conf import HOST, PORT, FILE
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((HOST, PORT))
-    server.listen(1)
+    server.listen(5)
 
-    print(f"Listening on {HOST}:{PORT}")
+    print(f"Listening on {HOST}:{PORT}", flush=True)
 
     while True:
         conn, addr = server.accept()
 
+        print(f"Connection from {addr}", flush=True)
+
         with conn:
-            print(f"Connection from {addr}")
+            chunks = []
 
-            data = conn.recv(1024)
+            while True:
+                data = conn.recv(1024)
 
-            if data:
-                message = data.decode("utf-8", errors="replace")
-                print(f"Received: {message}")
+                if not data:
+                    break
 
-                with open(FILE, "a") as f:
-                    f.write(message + "\n")
+                chunks.append(data)
+
+            message = b"".join(chunks).decode(
+                "utf-8",
+                errors="replace"
+            )
+
+            print(f"Received: {message}", flush=True)
+
+            with open(FILE, "a") as f:
+                f.write(message + "\n")
 
