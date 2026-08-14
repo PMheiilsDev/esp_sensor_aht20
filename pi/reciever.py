92098bd07e3b9cefc9fb624ff95afc0604175844
PM <>
2026-08-14 17:59:12 +0200

    reciever python script added

---
+++ pi/reciever.py
@@ -0,0 +1,28 @@
+import socket
+
+HOST = "0.0.0.0"
+PORT = 5000
+FILE = "/home/pi/data.txt"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen(1)
+
+    print(f"Listening on {HOST}:{PORT}")
+
+    while True:
+        conn, addr = server.accept()
+
+        with conn:
+            print(f"Connection from {addr}")
+
+            data = conn.recv(1024)
+
+            if data:
+                message = data.decode("utf-8", errors="replace")
+                print(f"Received: {message}")
+
+                with open(FILE, "a") as f:
+                    f.write(message + "\n")
+
